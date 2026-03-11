c2d65d47755257891e237d8c59467921e4673c90
Bruno <brunokristian003@gmail.com>
2026-03-11 17:44:35 +0100

    feat: add initial Vue component with a greeting message

---
+++ resources/js/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="min-h-screen flex items-center justify-center bg-[#FDFDFC] dark:bg-[#0a0a0a]">
+        <h1 class="text-2xl font-semibold text-[#1b1b18] dark:text-[#EDEDEC]">
+            Hello from Vue!
+        </h1>
+    </div>
+</template>
